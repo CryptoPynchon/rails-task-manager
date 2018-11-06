c0af50f20aab64934cc6f59f2d1811bb485caddc
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-06 14:18:38 -0500

    full functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,32 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:title, :details, :completed))
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.new(params.require(:task).permit(:title, :details, :completed))
+    @task.update(params.require(:task).permit(:title, :details, :completed))
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
 end
